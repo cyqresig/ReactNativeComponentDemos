14fc864684a085f61dd73900ee0c13f650fa9938
cyqresig <moonsunfall@aliyun.com>
2016-09-22 11:46:13 +0800

    updated usage

--- examples/react-native-smart-toast/toast-text.js
+++ examples/react-native-smart-toast/toast-text.js
@@ -33,7 +33,7 @@ class LoadingToast extends Component {
                     style={{margin: 10, height: 40, backgroundColor: 'red', borderRadius: 3, borderWidth: StyleSheet.hairlineWidth, borderColor: 'red', justifyContent: 'center',}}
                     textStyle={{fontSize: 17, color: 'white'}}
                 >
-                    show top
+                    show top (顶部显示)
                 </Button>
 
                 <Button
@@ -43,7 +43,7 @@ class LoadingToast extends Component {
                     style={{margin: 10, justifyContent: 'center', height: 40, backgroundColor: 'red', borderRadius: 3, borderWidth: StyleSheet.hairlineWidth, borderColor: 'red', justifyContent: 'center',}}
                     textStyle={{fontSize: 17, color: 'white'}}
                 >
-                    show center
+                    show center (中心显示)
                 </Button>
 
                 <Button
@@ -53,14 +53,14 @@ class LoadingToast extends Component {
                     textStyle={{fontSize: 17,  color: 'white'}}
 
                 >
-                    show bottom
+                    show bottom (底部显示)
                 </Button>
                 <Button
                     onPress={this._showFastToast}
                     style={{margin: 10, height: 40, backgroundColor: 'red', borderRadius: 3, borderWidth: StyleSheet.hairlineWidth, borderColor: 'red', justifyContent: 'center',}}
                     textStyle={{fontSize: 17, color: 'white'}}
                 >
-                    show fast
+                    show fast (快速显示)
                 </Button>
                 <Button
                     onPress={this._showFastToastAndAnimatedHide}
@@ -74,7 +74,7 @@ class LoadingToast extends Component {
                     style={{margin: 10, height: 40, backgroundColor: 'red', borderRadius: 3, borderWidth: StyleSheet.hairlineWidth, borderColor: 'red', justifyContent: 'center',}}
                     textStyle={{fontSize: 17, color: 'white'}}
                 >
-                    show immediate
+                    show immediate (立即显示)
                 </Button>
                 <Button
                     onPress={this._showImmediateToastAndAnimatedHide}
@@ -87,7 +87,7 @@ class LoadingToast extends Component {
                 <Toast
                     ref={ component => this._toast = component }
                     marginTop={64}>
-                    Unable to connect to app store
+                    Unable to connect to apple store
                 </Toast>
             </View>
         )
@@ -102,12 +102,14 @@ class LoadingToast extends Component {
     _showCenterToast = () => {
         this._toast.show({
             position: Toast.constants.gravity.center,
+            children: <View><Text style={{color: 'yellow'}}>Unalbe to download now</Text></View>
         })
     }
 
     _showBottomToast = () => {
         this._toast.show({
             position: Toast.constants.gravity.bottom,
+            children: 'Unalbe to upload now'
         })
     }
 
@@ -115,6 +117,7 @@ class LoadingToast extends Component {
         this._toast.show({
             position: Toast.constants.gravity.center,
             duration: 255,
+            children: 'Unable to connect to apple store'
         })
     }
 
@@ -122,6 +125,7 @@ class LoadingToast extends Component {
         this._toast.show({
             position: Toast.constants.gravity.center,
             duration: 255,
+            children: 'Unable to connect to google store',
             animationEnd : () => {
                 this._toast._toastAnimationToggle = this.setTimeout( () => {
                     this._toast.hide({
@@ -139,12 +143,14 @@ class LoadingToast extends Component {
         this._toast.show({
             position: Toast.constants.gravity.center,
             duration: 0,
+            children: 'Unable to connect to wifi',
         })
     }
     _showImmediateToastAndAnimatedHide = () => {
         this._toast.show({
             position: Toast.constants.gravity.center,
             duration: 0,
+            children: 'Unable to connect to wlan',
             animationEnd : () => {
                 this._toast._toastAnimationToggle = this.setTimeout( () => {
                     this._toast.hide({
